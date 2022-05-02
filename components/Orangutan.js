4e1ee84616b67fae6d798c9b93cebeb284c2b327
joojdev <jvpiovezan2905@gmail.com>
2022-05-02 20:26:28 -0300

    Added onClick change orangutan function.

--- components/Orangutan.js
+++ components/Orangutan.js
@@ -1,10 +1,22 @@
-// import Image from 'next/image'
-
+import { useState } from 'react'
 import styles from '@styles/Orangutan.module.css'
 
 function Orangutan() {
-  // return <Image src='/orangutan.png' className={styles.image} alt='Orangutan' width='100' height='100' />
-  return <img src='/orangutan.png' alt='Orangutan' className={styles.image} />
+  const [imageIndex, setImageIndex] = useState(0)
+
+  function handleOrangutanClick() {
+    if (imageIndex + 1 >= 3) return setImageIndex(0)
+    setImageIndex(imageIndex + 1)
+  }
+
+  return (
+    <img
+      src={`/images/orangutan${imageIndex}.png`}
+      alt='Orangutan'
+      className={styles.image}
+      onClick={handleOrangutanClick}
+    />
+  )
 }
 
 export default Orangutan
