514f3ca9486c45ab6cc786fe44af0529fd1f35b1
joojdev <jvpiovezan2905@gmail.com>
2022-05-03 20:35:01 -0300

    Changed image selector.

--- components/Orangutan.js
+++ components/Orangutan.js
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import styles from '@styles/Orangutan.module.css'
 
+const imageArray = [
+  'surprised-orangutan.png',
+  'swinging-orangutan.png',
+  'plushie-orangutan.png',
+  'stoned-fox.png',
+  'beta-dog.png',
+  'chad-dog.png'
+]
+
 function Orangutan() {
   const [imageIndex, setImageIndex] = useState(0)
 
   function handleOrangutanClick() {
-    if (imageIndex + 1 >= 3) return setImageIndex(0)
+    console.log(imageArray)
+    if (imageIndex + 1 >= imageArray.length) return setImageIndex(0)
     setImageIndex(imageIndex + 1)
   }
 
   return (
     <img
-      src={`/images/orangutan${imageIndex}.png`}
+      src={`/images/${imageArray[imageIndex]}`}
       alt='Orangutan'
       className={styles.image}
       onClick={handleOrangutanClick}
